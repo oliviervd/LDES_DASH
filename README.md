4b8d546f98d14a86d33e1619860a996fb2f0da45
huynslol <oliviervandhuynslager@gmail.com>
2021-08-04 19:06:43 +0200

    init

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-#LDES-DASH
+#LDES-DASH [work in progress]
 
 dashboard for monitoring data published on LDES.
+runs on DASH, fetches data stored in postgres. 
--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
-import cdata.postgresql as mod
+import psycopg2
 import plotly.graph_objs as go
+
+from config import *
+from utils import *
+
+# connect to postgres
+connect_postgres()
 
