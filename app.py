539d0082b1fb80f7a45afd88afa705fd6bc89e21
huynslol <oliviervandhuynslager@gmail.com>
2021-09-24 11:03:38 +0200

    add logo

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import dash
-import dash_core_components as dcc
+import dash_core_components as dbc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
@@ -7,7 +7,7 @@ import pandas as pd
 app = dash.Dash(__name__)
 
 app.layout = html.Div(children=[
-    html.Img(src='/assets/logo.jpg'),
+    html.Img(src='/assets/logo.jpg', className="logo"),
     html.H1(children="CoGhent: Dashboard", className="app-header--title")
 ])
 
